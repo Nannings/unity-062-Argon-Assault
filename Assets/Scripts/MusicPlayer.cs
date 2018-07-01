defd289414e75c412108842d12a43a6c86fd9128
Nannings <mrnannings@gmail.com>
2018-07-01 18:01:02 +0200

    c 80;

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("LoadFirstScene", 2);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
