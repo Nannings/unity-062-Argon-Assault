d5931b5ab0f17fec367e476e03a4e61793dd5d44
Nannings <mrnannings@gmail.com>
2018-07-02 17:10:56 +0200

    c 84;

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+    }
+}
