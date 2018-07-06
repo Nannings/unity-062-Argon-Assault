c678cb7ea75bd4330401e32745caf10d8ef32f7c
Nannings <mrnannings@gmail.com>
2018-07-06 20:54:13 +0200

    c 95;

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Start()
+    {
+
+        Invoke("LoadFirstScene", 2);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
