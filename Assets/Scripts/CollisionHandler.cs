0ab38005572ecafabd3357536cf1380e2fa73c30
Nannings <mrnannings@gmail.com>
2018-07-09 19:49:18 +0200

    c 98;

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("Particle effect")] [SerializeField] GameObject deathFX;
 
     private void OnTriggerEnter(Collider collision)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadLevel", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
         print("Player dying");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
