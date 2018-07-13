60b9620e8ea740de0af1abb40876172b8d23c92c
Nannings <mrnannings@gmail.com>
2018-07-13 17:44:39 +0200

    c 102;

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+
+    static Transform parent;
+
     private void Start()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
@@ -13,6 +17,13 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject FX = Instantiate(deathFX, transform.position, Quaternion.identity);
+        if (parent == null)
+        {
+            parent = new GameObject().transform;
+            parent.name = "Spawned FX";
+        }
+        FX.transform.SetParent(parent);
         Destroy(gameObject);
     }
 }
